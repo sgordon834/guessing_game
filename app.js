30c7e13e2d6abce9c9e9d384732d90ce4c68dd6a
sgordon834 <sgordon834@gmail.com>
2017-01-28 00:09:09 -0800

    N2 Update 1

--- app.js
+++ app.js
@@ -1,65 +1,85 @@
-<script>
-    // var userName = prompt('what\'s is your name?');
-    //
-    // alert('Hello ' + userName + '.  Thank you for visiting to my site!');
-    //
-    // var answerOne = prompt('What state was I born in?');
-    // var myAnswerOne = 'Washington';
-    // if (answerOne === myAnswerOne) {
-    //
-    //     alert('Yes! that is correct.');
-    //
-    // } else {
-    //
-    //     alert('That\'s not correct, I was born in Washington');
-    // }
-    //
-    // var answerTwo = prompt('What military branch did I serve in?');
-    // var myAnswerTwo = 'coast guard';
-    // if (answerTwo === myAnswerTwo) {
-    //
-    //     alert('You got it right!');
-    //
-    // } else {
-    //
-    //     alert('Sorry, that\'s wrong, I was in the Coast Guard');
-    // }
-    //
-    // var answerThree = prompt('Where am I a student at?');
-    // var myAnswerThree = 'code fellows';
-    // if (answerThree === myAnswerThree) {
-    //
-    //     alert('You are smart, right on!');
-    //
-    // } else {
-    //
-    //     alert('Oh no, that is incorrect.');
-    // }
-    //
-    // var answerThree = prompt('What do I aspire to be?');
-    // var myAnswerThree = 'developer';
-    // if (answerThree === myAnswerThree) {
-    //
-    //     alert('Way to go, that\'s right!');
-    //
-    // } else {
-    //
-    //     alert('Oh no, that is incorrect.');
-    // } -->
+// <!-- Script to obtain user input
 
-  //   var whichState = prompt('What is your favorite programming language?');
-  //   var userAnswer = ['js', 'python', 'java'];
-  //
-  //   //console.log(userAnswer.indexOf(whichState));
-  //   //
-  //   // userAnswer.indexof(javascript);
-  //   // userAnswer.indexof(python);
-  //   // userAnswer.indexof(java);
-  //   if (userAnswer.indexOf(whichState) > -1) {
-  //     alert('You\'re on your way to tech greatness!');
-  //
-  //   } else if (userAnswer.indexOf(whichState) === -1){
-  //     alert('Learn to code or stay in yesteryear!');
-  //     console.log('Programming is fundamental using coding skills!');
-  // }
-  // </script>
+
+// Start of N1 js. 4 questions
+
+var userName = prompt('what\'s is your name?');
+
+alert('Hello ' + userName + '.  Thank you for visiting to my site!');
+
+var answerOne = prompt('Is Washington the \'Show Me State\'?');
+var myAnswerOne = 'no'.toLowerCase();
+
+if (answerOne === myAnswerOne) {
+
+    alert('You got it right, you rock!');
+
+} else {
+
+    alert('Sorry, that\'s wrong, try again!');
+}
+
+var answerTwo = prompt('Does a cat purr?');
+var myAnswerTwo = 'yes'.toLowerCase();
+if (answerTwo === myAnswerTwo) {
+
+    alert('Right again!');
+
+} else {
+
+    alert('Sorry, that\'s wrong, maybe next time!');
+}
+
+var answerThree = prompt('Was President Reagan an actor?');
+var myAnswerThree = 'yes'.toLowerCase();
+if (answerThree === myAnswerThree) {
+
+    alert('You are a genious, right on!');
+
+} else {
+
+    alert('Oh no, that is incorrect.');
+}
+
+var answerThree = prompt('Are lions purple?');
+var myAnswerThree = 'no'.toLowerCase();
+if (answerThree === myAnswerThree) {
+
+    alert('Way to go, that\'s right!');
+
+} else {
+
+    alert('Oh no, that is incorrect.');
+}
+
+// Start of N2 Array and Comparators. 2 questions.
+
+// Array question
+
+var whichLang = prompt('What is your favorite programming language?');
+var userAnswer = ['js', 'python', 'java', 'c'];
+
+if (userAnswer.indexOf(whichLang) > -1) {
+    alert('You\'re on your way to tech greatness!');
+    console.log('Programming is fundamental using coding skills!');
+
+} else if (userAnswer.indexOf(whichLang) === -1) {
+    alert('Learn to code or stay in yesteryear!');
+    console.log('Try out Code Fellows to exercise your brain!')
+
+}
+
+//  Logical Or question
+
+var answerOne = prompt('Is the moon round? (y or yes/n or no)');
+var myAnswerOne = 'yes'.toLowerCase();
+var myAnswerTwo = 'y'.toLowerCase();
+
+if (answerOne === myAnswerOne || answerOne === myAnswerTwo) {
+
+    alert('Whoopee! that is correct.');
+
+} else {
+
+    alert('That\'s not correct, try again.');
+}
