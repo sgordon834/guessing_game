5a06ae71f5e85f1a9d51d6f32faa243f472f2813
sgordon834 <sgordon834@gmail.com>
2017-01-28 22:38:58 -0800

    Update app.js question_seven

--- app.js
+++ app.js
@@ -1,85 +1,122 @@
 // Script to obtain user input
 // Start of N1 js. 4 questions
+//
+// var userName = prompt('what\'s is your name?');
+//
+// alert('Hello ' + userName + '.  Thank you for visiting to my site!');
+//
+// console.log(userName);
+//
+// var answerOne = prompt('Is Washington the \'Show Me State\'?').toLowerCase();
+// var myAnswerOne = 'no';
+//
+// if (answerOne === myAnswerOne) {
+//
+//     alert('You got it right, you rock!');
+//
+// } else {
+//
+//     alert('Sorry, that\'s wrong, try again!');
+// }
+//
+// var answerTwo = prompt('What do I collect as a hobby?')();
+// var myAnswerTwo = 'comics';
+// if (answerTwo === myAnswerTwo) {
+//
+//     alert('Right again!');
+//
+// } else {
+//
+//     alert('Sorry, that\'s wrong, maybe next time!');
+// }
+//
+// var answerThree = prompt('What color is my SUV?').toLowerCase();
+// var myAnswerThree = 'tan';
+// if (answerThree === myAnswerThree) {
+//
+//     alert('You are a genious, right on!');
+//
+// } else {
+//
+//     alert('Oh no, that is incorrect.');
+// }
+//
+// var answerThree = prompt('Who is Mario's girlfriend from Donkey Kong?').toLowerCase();
+// var myAnswerThree = 'pauline';
+// if (answerThree === myAnswerThree) {
+//
+//     alert('Way to go, that\'s right!');
+//
+// } else {
+//
+//     alert('Oh no, that is incorrect.');
+// }
+//
+// // Start of N2 Array and Comparators. 2 questions.
+//
+// // Array question
+//
+// var whichLang = prompt('What is your favorite programming language?');
+// var userAnswer = ['js', 'python', 'java', 'c'];
+//
+// if (userAnswer.indexOf(whichLang) > -1) {
+//     alert('You\'re on your way to tech greatness!');
+//     console.log('Programming is fundamental using coding skills!');
+//
+// } else if (userAnswer.indexOf(whichLang) === -1) {
+//     alert('Learn to code or stay in yesteryear!');
+//     console.log('Try out Code Fellows to exercise your brain!')
+//
+// }
+//
+// //  Logical Or question
+//
+// var answerOne = prompt('Is the moon round? (y or yes/n or no)').toLowerCase();
+// var myAnswerOne = 'yes';
+// var myAnswerTwo = 'y';
+//
+// if (answerOne === myAnswerOne || answerOne === myAnswerTwo) {
+//
+//     alert('Whoopee! that is correct.');
+//
+// } else {
+//
+//     alert('That\'s not correct, try again.');
+// }
+//
+// var corrNumb = (42).toString();
+// var userNumb = prompt('Select a number between 1 and 100');
+// console.lgo('HELLO~~~~~~~~~');
+//
+// if(userNumb > corrNumb){
+//   userNumb = prompt('That\s too high!');
+// }else if(userNumb < corrNumb){
+//   userNumb = prompt('That\'s too low!');
+// }else(userNumb === corrNumb) {
+//   alert('Bingo!');
 
-var userName = prompt('what\'s is your name?');
 
-alert('Hello ' + userName + '.  Thank you for visiting to my site!');
 
-console.log(userName);
+var num = 14;
+var corrNumb = num.toString();
+var countAtt = 0;
 
-var answerOne = prompt('Is Washington the \'Show Me State\'?').toLowerCase();
-var myAnswerOne = 'no';
+while(countAtt < 4){
+  countAtt++;
+  var userNumb = prompt('Select a number between 1 - 25 (you have 4 guesses).');
 
-if (answerOne === myAnswerOne) {
+  if(userNumb > corrNumb){
+    alert('That\'s too high, try again.');
 
-    alert('You got it right, you rock!');
+  }if(userNumb < corrNumb){
+    alert('That\'s too low, try again');
 
-} else {
+  }if(userNumb === corrNumb) {
+    alert('That\'s right!');
+    break;
 
-    alert('Sorry, that\'s wrong, try again!');
+  }if(countAtt === 4) {
+    alert('Sorry that was the last guess.');
+  }
 }
-
-var answerTwo = prompt('Does a cat purr?').toLowerCase();
-var myAnswerTwo = 'yes';
-if (answerTwo === myAnswerTwo) {
-
-    alert('Right again!');
-
-} else {
-
-    alert('Sorry, that\'s wrong, maybe next time!');
-}
-
-var answerThree = prompt('Was President Reagan an actor?').toLowerCase();
-var myAnswerThree = 'yes';
-if (answerThree === myAnswerThree) {
-
-    alert('You are a genious, right on!');
-
-} else {
-
-    alert('Oh no, that is incorrect.');
-}
-
-var answerThree = prompt('Are lions purple?').toLowerCase();
-var myAnswerThree = 'no';
-if (answerThree === myAnswerThree) {
-
-    alert('Way to go, that\'s right!');
-
-} else {
-
-    alert('Oh no, that is incorrect.');
-}
-
-// Start of N2 Array and Comparators. 2 questions.
-
-// Array question
-
-var whichLang = prompt('What is your favorite programming language?');
-var userAnswer = ['js', 'python', 'java', 'c'];
-
-if (userAnswer.indexOf(whichLang) > -1) {
-    alert('You\'re on your way to tech greatness!');
-    console.log('Programming is fundamental using coding skills!');
-
-} else if (userAnswer.indexOf(whichLang) === -1) {
-    alert('Learn to code or stay in yesteryear!');
-    console.log('Try out Code Fellows to exercise your brain!')
-
-}
-
-//  Logical Or question
-
-var answerOne = prompt('Is the moon round? (y or yes/n or no)').toLowerCase();
-var myAnswerOne = 'yes';
-var myAnswerTwo = 'y';
-
-if (answerOne === myAnswerOne || answerOne === myAnswerTwo) {
-
-    alert('Whoopee! that is correct.');
-
-} else {
-
-    alert('That\'s not correct, try again.');
-}
+console.log(userNumb);
